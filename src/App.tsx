ccfe1a65323c8d0e50bb56d36d8a2c0929ebd834
Thiago <ps-thiago@hotmail.com>
2022-08-29 15:15:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/Button'
+
+export function App() {
+  return (
+    <>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </>
+  )
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+interface ButtonProps {
+  color?: 'primary' | 'secondary' | 'danger' | 'sucess'
+}
+
+export function Button(button: ButtonProps) {
+  return <button>Enviar</button>
+}
